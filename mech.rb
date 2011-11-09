bdc29de2f0ff48efcf8881d7ed6eaf5e4fbe196e
Kazantsev Nickolay <kazantsev.nickolay@gmail.com>
2011-11-09 20:35:14 +0400

    add yaml structure of data and default merge mechanism

--- mech.rb
+++ mech.rb
@@ -1,5 +1,14 @@
 require 'find'
 require 'yaml'
+require 'psych'
+
+unless {}.respond_to?(:deep_merge)
+  class Hash
+    def deep_merge(*args)
+      merge(*args)
+    end
+  end
+end
 
 module Mech
 
@@ -46,27 +55,47 @@ module Mech
 
     def build_merge_graph
       @paths_with_deep = @paths.map do |path|
-        otn_path = path.gsub(config.src_path, '')
-        path_deep = otn_path.split('/').size
-        [otn_path, path_deep]
+        path_deep = path.split('/').size
+        [path, path_deep]
       end
       @paths_with_deep.sort_by! { |p| p[1] }
+      @min_path_deep = @paths_with_deep.first[1]
+      @max_path_deep = @paths_with_deep.last[1]
     end
 
-    def type_merge(path)
-      path
+    def build_extend_object(path, pattern)
+      paths = path.split('/')
+      paths.pop
+      path = paths.join('/') + '.yml'
+
+      if File.exist?(path)
+        obj = Psych.load(File.read(path))
+        pattern = pattern.deep_merge(obj || {})
+      end
+
+      if path =~ Regexp.new(config.src_path)
+        pattern = build_extend_object(path, pattern)
+      end
+
+      pattern
     end
 
     def merge_recursive
       @paths_with_deep.delete_if { |p| p[0] == '/common.yml' }
       @common_obj = YAML.load(File.read(config.src_path + '/common.yml'))
 
-      type_index = @paths_with_deep[0][1]
-      @types = @paths_with_deep.select { |y| y[1] == type_index }.map(&:first)
-      @paths_with_deep.delete_if { |y| y[1] == type_index }
-      @paths = @paths_with_deep.map(&:first)
+      @stored_paths = @paths_with_deep.select { |p, d| d == @max_path_deep }.map(&:first)
 
-      @types.map { |type_path| type_merge(type_path) }
+      merged_items_by_path = @stored_paths.map do |stored_path|
+        extend_obj = build_extend_object(stored_path, {})
+        objs = Psych.load(File.read(stored_path))
+
+        objs.each do |name, obj|
+          objs[name] = @common_obj.deep_merge(extend_obj.deep_merge(obj))
+        end
+
+        objs
+      end
     end
 
     def construct_data
